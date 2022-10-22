1a350329ceeedc6a048ee85da43b06a1e0cab4e8
Reza Sam <s_gomeishi@yahoo.com>
2022-10-22 14:05:10 +0330

    added file

---
+++ S01/2.py
@@ -0,0 +1,8 @@
+pi=22/7
+radian_=float(input('give me a number '))
+height_=float(input('give me a number '))
+hajm_=height_*radian_**2*pi
+masahate_kol=(2*pi*radian_*height_)+(2*(pi*(radian_)**2))
+
+print(hajm_,masahate_kol)
+
